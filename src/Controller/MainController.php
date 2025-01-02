6061954423c98057480f4c0ae6e57162f7aaaf6a
Angeanddemon60 <l.georgelin@hotmail.fr>
2025-01-02 18:01:47 +0100

    translation and factorisation

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\AboutRepository;
+use App\Repository\ContactRepository;
+use App\Repository\FaqRepository;
+use App\Repository\FeatureRepository;
+use App\Repository\HeroRepository;
+use App\Repository\ServiceRepository;
+use App\Repository\ValueRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(AboutRepository $aboutRepository, ContactRepository $contactRepository, FaqRepository $faqRepository, FeatureRepository $featureRepository, HeroRepository $heroRepository, ServiceRepository $serviceRepository, ValueRepository $valueRepository): Response
     {
         return $this->render('main/index.html.twig', [
-            /* 'controller_name' => 'MainController', */
+            'title' => "Accueil",
+            'hero' => $heroRepository->findAll(),
+            'about' => $aboutRepository->findAll(),
+            'contact' => $contactRepository->findAll(),
+            'faqs' => $faqRepository->findAll(),
+            'features' => $featureRepository->findAll(),
+            'services' => $serviceRepository->findAll(),
+            'values' => $valueRepository->findAll(),
         ]);
     }
 }
